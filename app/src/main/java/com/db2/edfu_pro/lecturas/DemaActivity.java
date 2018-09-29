f53fb211ff5f5499e3e53e69b560625a898a307d
eduardo19Fu <ramieduar@gmail.com>
2018-09-28 20:30:45 -0600

    Cierre version 0.2

---
+++ app/src/main/java/com/db2/edfu_pro/lecturas/DemaActivity.java
@@ -0,0 +1,13 @@
+package com.db2.edfu_pro.lecturas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DemaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dema);
+    }
+}
